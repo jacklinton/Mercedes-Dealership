58e284c177de2c8a62e2c34618d94a497ee56cc5
Jack Linton <jacklinton@mac.com>
2017-02-06 23:10:00 -0500

    Setup the environmental variable for the API key and successfully tested it. Also started building the framework for the input fields on the contacts page.

--- main.rb
+++ main.rb
@@ -1,2 +1,17 @@
-require 'sinatra'
+#require 'sinatra'
 require 'sendgrid-ruby'
+
+from = SendGrid::Email.new(email: 'Jacklinton@mac.com')
+subject = 'Hello World from the SendGrid Ruby Library!'
+to = SendGrid::Email.new(email: 'molecles@gmail.com')
+
+content = SendGrid::Content.new(type: 'text/plain', value: 'Hello, Email!')
+
+mail = SendGrid::Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+puts response.status_code
+puts response.body
