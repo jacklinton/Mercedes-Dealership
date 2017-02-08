376a26469531de12cc16a7f458d1a808d8d818ff
Jack Linton <jacklinton@mac.com>
2017-02-08 15:59:52 -0500

    The contacts page is complete and fully functional. It needs some simple styling, but otherwise it works fl;awlessly. Also time to start thinking about how to integrate other people's pages.

--- main.rb
+++ main.rb
@@ -7,42 +7,30 @@ post '/' do
 	@full_name = params["full-name"]
 	@email_addy = params["email-addy"]
 	@email_addy2 = params["email-add2"]
-	@phone = params["phone"]
+	@phone = "Customer phone: " + params["phone"]
 	@email_body = params["email-body"]
-	@pref_email = params["pref-email"]
-	@pref_phone = params["'pref-phone"]
-	@data_string = [@full_name, @email_addy, @email_addy2, @phone, @email_body]
-	@warning = "You must complete all fields to proceed!"
+	#@pref_email = params["pref-email"]
+	#@pref_phone = params["pref-phone"]
+	
 
-	#  def checker
-	#  	@data_string.each do |input|
-	#  		if input != nil
-	#  	    else
-	# # 	    	display warning
-	# 			checker
-	# 		end
-	# 	end
-	# 	checker2
-	# end
-	# def checker2
-	# 	if @email_addy == @email_addy2
-	# 		sender
-	# 	else
-	# 		#display warning
-	# 		checker2
-	# 	end
-	# end
-	# def sender
+	if params["pref-email"] == 'on'
+		@pref_email = 'Prefers email communication.'
+	end
+	if params["pref-phone"] == 'on'
+		@pref_phone = 'Prefers phone communication.'
+	end
+
+	@data_string = [@phone, @pref_phone, @pref_email, @email_body].join(", ")
+
 		from = SendGrid::Email.new(email: @email_addy)
 		subject = 'New customer message from ' + @full_name
 		to = SendGrid::Email.new(email: 'mercedes.sales.and.inquiries@gmail.com') #pw: jackashotsohamjon
-		content = SendGrid::Content.new(type: 'text/plain', value: @email_body)
+		content = SendGrid::Content.new(type: 'text/plain', value: @data_string)
 		mail = SendGrid::Mail.new(from, subject, to, content)
 		sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 		response = sg.client.mail._('send').post(request_body: mail.to_json)
 		puts response.status_code
 		puts response.body
-# 	end
 end
 
 get '/' do
--- public/scripts/contacts.js
+++ public/scripts/contacts.js
@@ -7,6 +7,76 @@ var prefEmail = document.getElementById("pref-email") //focus6
 var prefPhone = document.getElementById("pref-phone") //focus7
 var submitButton = document.getElementById("submit-button") //focus8
 
-$("#email-body").focusin(function(){
+submitButton.disabled = true
+
+$("#email-body").focusin(function() {
+	if (checkEmail() && emailAddy.value == emailAddy2.value) {
+		submitButton.disabled = false
+	} else {
+		submitButton.disabled = true
+	}
+})
+
+$("#email-body").focusin(function(){ // empties text field when it comes into focus
 	$("#email-body").text('')
 })
+
+$("#email-addy").focusout(function(){
+	checkEmail()
+})
+
+$("#email-addy2").focusout(function(){
+	if (emailAddy.value != emailAddy2.value) {
+		$("#emailWarning2").text("Your email addresses must match!")
+	}
+	else {
+		$("#emailWarning2").text("")
+	}
+	 
+})
+
+
+checkEmail = function() {
+		this.emailArr = emailAddy.value.split("@")
+		this.emailArr1 = this.emailArr[0]				//splits the email up into sections with "@", ".", the first character as dividers. 
+		
+		
+
+		var first = this.emailArr1.split("")
+		//second = emailArr3.split("")
+		//third = emailArr4.split("")
+
+		var firstChar = first[0]
+
+		if (this.emailArr.length != 2) {
+			$("#emailWarning1").text("The email address must contain an '@' symbol!")	//A series of checks for the checkpoints above. Throws an alert if the email is invalid in some way.
+			return false
+		}
+		this.emailArr2 = this.emailArr[1].split(".")
+
+		if (this.emailArr2.length != 2) {							//check that there are at least two word characters on either side of the period.
+			$("#emailWarning1").text("A valid email address must have a '.' after the '@'!")
+			return false
+		}
+		this.emailArr3 = this.emailArr2[0]
+		this.emailArr4 = this.emailArr2[1]
+
+		if (!isNaN(firstChar)) {								//Makes sure email doesn't start with a number.
+			$("#emailWarning1").text("A valid email cannot start with a number!")
+			return false
+
+		} else {
+			valid1 = /[a-z0-9_]+/i
+			valid2 = /[a-z0-9_-]+/i
+
+			if (this.emailArr1.match(valid1) && this.emailArr3.match(valid2) && this.emailArr4.match(valid1)) { //Takes the remaining pieces of the email address 																								
+				$("#emailWarning1").text("")
+				return true																						//and checks to make sure it only uses acceptible characters.
+
+			} else {																							//From http://regexr.com/
+				$("#emailWarning1").text("This email contains invalid characters!")
+				return false
+
+			}	
+		}
+}
